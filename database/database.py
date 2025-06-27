c76c04ed578141484ef702a5455f4962fc95d78d
Keva1z <Keva1z@yandex.ru>
2025-06-27 22:12:50 +0400

    feat(database): Fully implemented database

--- database/database.py
+++ database/database.py
@@ -1,17 +1,49 @@
-import os, json
+import pathlib, json
+from database.models import Task
 
-class database():
-    __path: str = os.path.expanduser("~/tasks.json")
-    __objects: dict[str, str] = {}
+class JSONDatabase():
+    __path: str = pathlib.Path("database/tasks.json")
+    __objects: dict[int, Task] = {}
 
     def __init__(self) -> None:
-        pass
+        self.__load()
 
-    def put(self, key: str, object) -> None:
-        pass
+    def put(self, task: Task) -> None:
+        self.__objects[task.id] = task
     
-    def get(self, key: str) -> str|None:
-        return self
+    def get(self, key: int|None = None) -> Task|list[Task]|None:
+        if key is not None: return self.__objects[key] if key in self.__objects else None
+        return list(self.__objects.values())
     
-    def update(self, key: str) -> None:
-        pass
+    def update(self, task: Task) -> None:
+        if task.id in self.__objects:
+            self.__objects[task.id] = task
+
+    def delete(self, key: int) -> Task|None:
+        if key in self.__objects:
+            if Task._object_count > key:
+                change = False
+                for obj_key, task in self.__objects.items():
+                    if obj_key > key: change = True
+                    if change:
+                        task.id -= 1
+                        self.__objects[obj_key-1] = task
+            object = self.__objects.pop(Task._object_count)
+            Task._object_count -= 1
+            return object
+        return None
+
+    def save(self) -> None:
+        with open(self.__path, 'w+', encoding='UTF-8') as f:
+            objects = {}
+            for key, task in self.__objects.items(): objects[key] = task.to_dict()
+            f.write(json.dumps(objects, indent=4))
+
+    def __load(self) -> None:
+        with open(self.__path, 'r+', encoding='UTF-8') as f:
+            try:
+                objects: dict[int, str] = json.loads(f.read())
+                for key, object in objects.items():
+                    self.__objects[int(key)] = Task.from_dict(object)
+            except:
+                pass
--- database/models.py
+++ database/models.py
@@ -1,5 +1,8 @@
 from datetime import datetime, timezone
 from enum import Enum
+from typing import Any
+
+time_format = "%Y-%m-%dT%H:%M:%S"
 
 class Status(Enum):
     NOT_DONE = 'not-done'
@@ -7,16 +10,45 @@ class Status(Enum):
     DONE = 'done'
 
 class Task:
-    __task_count = 0
+    _object_count = 0
 
     def __init__(self, description: str, *args, **kwargs):
         
-        Task.__task_count += 1
-        self.id: int = self.__task_count
+        Task._object_count += 1
+        self.id: int = self._object_count
         self.status: Status = Status.NOT_DONE
         self.description: str = description
-        self.created_at: str = str(datetime.now(timezone()))
+        self.created_at: str = str(datetime.now())
         self.updated_at: str = self.created_at
 
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        object.__setattr__(self, name, value)
+        if name not in {"updated_at", "created_at"}:
+            object.__setattr__(self, "updated_at", str(datetime.now()))
+
+    @classmethod
+    def from_dict(cls, data: dict) -> 'Task':
+        return cls(
+            description = data.get('description', ''),
+            status = Status(data.get('status', 'not-done')),
+            created_at = data.get('created_at', ''),
+            updated_at = data.get('updated_at', '')
+        )
+
     def to_dict(self) -> dict:
-        return {}
+        return {
+            'status': self.status.value,
+            'description': self.description,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
+    
+    def __str__(self) -> str:
+        return f'{self.id}. {self.description} ({self.status.value})'
+    
+    def __repr__(self) -> str:
+        # return f'Task(id={self.id})'
+        return str(self)
